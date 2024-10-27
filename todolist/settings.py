bfbce5426156aac747cc53d311b227bf0a9491af
Enzo <enzopezzoni@icloud.com>
2024-10-26 21:14:37 -0400

    CSRF token encore et toujours

--- todolist/settings.py
+++ todolist/settings.py
@@ -156,3 +156,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_DOMAIN = ".azurewebsites.net"
